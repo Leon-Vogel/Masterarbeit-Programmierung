ae1e8fcb5122ad5f2ab7e39841616e62344ac118
leonv <leon.vogel@fh-bielefeld.de>
2024-06-17 10:00:08 +0200

    Cluster Count multiplot

--- isri_optimizer/rl_sequential_agent/plotting_cluster.py
+++ isri_optimizer/rl_sequential_agent/plotting_cluster.py
@@ -25,6 +25,7 @@ def cluster_size_metrics(df_cluster, k_range, cluster):
     silhouette = []
     davies_bouldin = []
     for k in k_range:
+        print(k)
         if cluster == 'Kmeans':
             model = KMeans(n_clusters=k).fit(df_cluster)
             y_means = model.predict(df_cluster)
@@ -57,7 +58,7 @@ def plot_elbow_method(k_range, values, xlabel, ylabel, title, file_path, file_na
         'figure.dpi': dpi,
         'figure.figsize': fig_size
     })
-    plt.plot(k_range, values, 'bx-')
+    plt.plot(k_range, values, 'b-')
     plt.xlabel(xlabel)
     plt.ylabel(ylabel)
     # plt.title(title, fontsize=font_size, pad=10)
@@ -68,6 +69,45 @@ def plot_elbow_method(k_range, values, xlabel, ylabel, title, file_path, file_na
     plt.savefig(f"{file_path}{file_name}_{file_suffix}.svg", format='svg', dpi=dpi)
     plt.savefig(f"{file_path}{file_name}_{file_suffix}.pdf", format='pdf', dpi=dpi)
     plt.close()
+
+
+def plot_elbow_method_multi(x_data_list, y_data_list, titles, sup_title, x_label, y_labels, 
+                            file_path, file_name, file_suffix, 
+                            show_sup_title=True, show_titles=True,
+                            x_scale='linear', y_scale='linear', line_style=None,
+                            font_size=8, dpi=500, fig_size=None):
+    if fig_size is None:
+        figsize=(4.5, 4)
+    plt.rcParams.update({
+        'font.size': font_size,
+        'legend.fontsize': font_size,
+        'xtick.labelsize': font_size,
+        'ytick.labelsize': font_size,
+        'font.family': 'Times New Roman'
+    })
+    fig, axs = plt.subplots(2, 2, figsize=figsize)
+    for plot_n, (ax, x_data, y_data, title, y_label) in enumerate(zip(axs.flatten(), x_data_list, y_data_list, titles, y_labels), start=1):
+        ax.set_xscale(x_scale)
+        ax.set_yscale(y_scale)
+        ax.grid(True)
+        ax.set_xticks(range(min(x_data), max(x_data)+1, 2))
+        if show_titles:
+            ax.set_title(title, fontsize=font_size)
+        ax.set_ylabel(y_label, fontsize=font_size)
+        ax.plot(x_data, y_data, 'b-')
+    for ax in axs[1,:]:
+        ax.set_xlabel(x_label, fontsize=font_size)
+    if show_sup_title:
+        fig.suptitle(sup_title, fontsize=font_size)
+        plt.subplots_adjust(top=0.9)
+
+    plt.tight_layout()
+
+    plt.savefig(f"{file_path}{file_name}_{file_suffix}.png", format='png', dpi=dpi)
+    plt.savefig(f"{file_path}{file_name}_{file_suffix}.svg", format='svg', dpi=dpi)
+    plt.savefig(f"{file_path}{file_name}_{file_suffix}.pdf", format='pdf', dpi=dpi)
+    plt.close()
+        
 
 def plot_hist(all_labels, bins, xlabel, ylabel, title, file_path, file_name, file_suffix, font_size=8, dpi=500, fig_size=None, grid=True):
     all_labels = [x + 1 for x in all_labels]
@@ -135,6 +175,34 @@ def plot_relative_hist(all_labels, bins, xlabel, ylabel, title, file_path, file_
     plt.savefig(f"{file_path}{file_name}_{file_suffix}.pdf", format='pdf', dpi=dpi)
     plt.close()
 
+def plot_relative_hist_3x2(ax, all_labels, bins, xlabel, ylabel, title, font_size=8, grid=True, every_nth=1):
+    all_labels = [x + 1 for x in all_labels]
+    plt.rcParams.update({
+        'font.size': font_size,
+        'legend.fontsize': font_size,
+        'xtick.labelsize': font_size,
+        'ytick.labelsize': font_size,
+        'font.family': 'Times New Roman'
+    })
+    # Berechne die relativen Häufigkeiten
+    counts, bin_edges = np.histogram(all_labels, bins=np.arange(0.5, bins + 1.5, 1))
+    relative_freqs = counts / len(all_labels)
+    
+    ax.hist(all_labels, bins=np.arange(0.5, bins + 1.5, 1), weights=np.ones_like(all_labels) / len(all_labels), edgecolor='black')
+    ax.set_xticks(np.arange(1, bins+1, 1))
+    if xlabel !='':
+        ax.set_xlabel(xlabel)
+    if col == 0:
+        ax.set_ylabel(ylabel)
+    ax.set_title(title, fontsize=font_size)
+    if grid:
+        ax.grid(True)
+        ax.set_axisbelow(True)
+    if every_nth > 1:
+        for n, label in enumerate(ax.xaxis.get_ticklabels()):
+            if n % every_nth != 0:
+                label.set_visible(False)
+
 def create_histogram_data_table(data, experiment_type, output_path):
     df = pd.DataFrame(data, columns=['Shift'])
     df['Shift'] = df['Shift'] + 1
@@ -182,13 +250,22 @@ if plot_cluster_metrics:
     for Methode in Methoden:
         distortions, inertias, silhouette, davies_bouldin = cluster_size_metrics(df_cluster, K, Methode)
         data = [distortions, inertias, silhouette, davies_bouldin]
+        if Methode == 'Kmeans':
+            kmeans_data = data
         Titel = [f'Die Ellenbogen-Methode mit Distortion für {Methode} Clustering', f'Die Ellenbogen-Methode mit Inertia für {Methode} Clustering', 
             f'Der Silhouette Score für {Methode} Clustering', f'Der Davies Bouldin Score für {Methode} Clustering']
         for i in range(len(data)):
             plot_elbow_method(K, data[i], 'Anzahl der Klassen', Achsen[i], Titel[i], 
                         'isri_optimizer/rl_sequential_agent/plots/data/', Methode, datei_endung[i])
+    titel_multi = ['Kmeans', 'Kmeans', 'Agglomerativ', 'Agglomerativ']
+    plot_elbow_method_multi([K, K, K, K], [kmeans_data[0], kmeans_data[2], data[0], data[2]], 
+                            titles=titel_multi, sup_title=None, 
+                            x_label='Anzahl der Klassen', y_labels=['Varianz innerhalb der Cluster', 'Silhouettenkoeffizient', 'Varianz innerhalb der Cluster', 'Silhouettenkoeffizient'],
+                            file_path='isri_optimizer/rl_sequential_agent/plots/data/', file_name='Cluster_Kennzahlen_Multiplot', 
+                            file_suffix='2x2', show_sup_title=False, show_titles=True
+    )
 
-plot_cluster_count = True
+plot_cluster_count = False
 if plot_cluster_count:
     GA_SOLUTIONS_PATH = "./isri_optimizer/rl_sequential_agent/IsriDataset.pkl"
     path="./isri_optimizer/rl_sequential_agent/cluster_models/"
@@ -212,6 +289,50 @@ if plot_cluster_count:
                 #          'isri_optimizer/rl_sequential_agent/plots/data/count/', f'{Methode}_Count', f'n{i}')
                 plot_relative_hist(all_labels, i, 'Klassen', 'relative Häufigkeit', title=f'{Namen[counter]} Clustering', 
                           file_path='isri_optimizer/rl_sequential_agent/plots/data/count/', file_name=f'{Namen[counter]}_Count', file_suffix=f'n{i}')
+
+plot_cluster_count_3x2 = True
+if plot_cluster_count_3x2:
+    GA_SOLUTIONS_PATH = "./isri_optimizer/rl_sequential_agent/IsriDataset.pkl"
+    path="./isri_optimizer/rl_sequential_agent/cluster_models/"
+    n_cluster = [8, 12, 15]
+    isri_dataset = pickle.load(open(GA_SOLUTIONS_PATH, 'rb'))
+    Methoden = ['kmeans', 'knn']
+    Namen = ['Kmeans', 'Agglomeratives']
+    plt.rcParams.update({
+        'font.size': 8,
+        'legend.fontsize': 8,
+        'xtick.labelsize': 8,
+        'ytick.labelsize': 8,
+        'font.family': 'Times New Roman'
+    })
+    fig, axs = plt.subplots(len(n_cluster), len(Methoden), figsize=(4.5, 6))
+    #fig.subplots_adjust(hspace=0.4, wspace=0.4)
+    
+    for row, i in enumerate(n_cluster):
+        for col, Methode in enumerate(Methoden):
+            if os.path.exists(f"{path}{Methode}_model_n{i}.pkl"):
+                with open(f"{path}{Methode}_model_n{i}.pkl", "rb") as f:
+                    model = pickle.load(f)
+                all_labels = []
+                for test_id in range(606):
+                    times_array = [j['times'] for j in isri_dataset.data['Jobdata'][test_id].values()]
+                    labels = model.predict(times_array)
+                    all_labels.append(labels)
+                all_labels = np.array(all_labels).flatten()
+                
+                ax = axs[row, col]
+                if row == 0:
+                    plot_relative_hist_3x2(ax, all_labels, i, '', 'relative Häufigkeit', title=f'{Namen[col]} Clustering')
+                elif row == 2:
+                    plot_relative_hist_3x2(ax, all_labels, i, 'Klassen', 'relative Häufigkeit', title='', every_nth=2)
+                else:
+                    plot_relative_hist_3x2(ax, all_labels, i, '', 'relative Häufigkeit', title='')
+    plt.tight_layout()
+    plt.savefig('isri_optimizer/rl_sequential_agent/plots/data/count/cluster_histograms.png', format='png', dpi=500)
+    plt.savefig('isri_optimizer/rl_sequential_agent/plots/data/count/cluster_histograms.svg', format='svg', dpi=500)
+    plt.savefig('isri_optimizer/rl_sequential_agent/plots/data/count/cluster_histograms.pdf', format='pdf', dpi=500)
+    plt.close()
+
 
 plot_GA_solution = False
 if plot_GA_solution:
